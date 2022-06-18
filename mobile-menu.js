68d5efb1133695146ea207ca01cd54956412f569
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-06-18 23:06:53 +0200

    add miscellanous changes

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,11 +1,13 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu');
 const mobileMenu = document.querySelector('#mobile-menu');
-const menuClose = document.querySelector('#mobile-menu i')
+const menuClose = document.querySelector('#mobile-menu i');
+const header = document.querySelector('header')
 
 function toggleMenu(button) {
   button.addEventListener('click' , () => {
     mobileMenu.classList.toggle('display-none');
     document.body.classList.toggle('no-scroll');
+    header.classList.toggle('display-none')
   })
 }
 
